82604dfdddddd4a1fca0779d60cf843b7d2cad12
Enzo <portet.lasserre.enzo@gmail.com>
2014-10-23 15:14:38 +0200

    initial commit

---
+++ Veloose/Veloose.Shared/Model/BikeModels.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veloose.Model
+{
+    public class Properties
+    {
+        public string name { get; set; }
+    }
+
+    public class Crs
+    {
+        public string type { get; set; }
+        public Properties properties { get; set; }
+    }
+
+    public class Geometry
+    {
+        public string type { get; set; }
+        public List<double> coordinates { get; set; }
+    }
+
+    public class Properties2
+    {
+        public string nom { get; set; }
+        public int num_station { get; set; }
+        public int nb_bornettes { get; set; }
+        public string En_service { get; set; }
+        public string M_en_S_16_nov_07 { get; set; }
+        public string street { get; set; }
+        public string Mot_Directeur { get; set; }
+        public string commune { get; set; }
+        public string code_insee { get; set; }
+        public double X_CC43 { get; set; }
+        public double Y_CC43 { get; set; }
+        public double X_WGS84 { get; set; }
+        public double Y_WGS84 { get; set; }
+        public string Nrivoli { get; set; }
+        public string No { get; set; }
+    }
+
+    public class Feature
+    {
+        public string type { get; set; }
+        public Geometry geometry { get; set; }
+        public Properties2 properties { get; set; }
+    }
+
+    public class RootObject
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+        public Crs crs { get; set; }
+        public List<Feature> features { get; set; }
+    }
+}
---
+++ Veloose/Veloose.Shared/ViewModel/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using GalaSoft.MvvmLight;
+
+namespace Veloose.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private string _helloWorld;
+
+        public string HelloWorld
+        {
+            get { return _helloWorld; }
+            set { Set(() => HelloWorld, ref _helloWorld, value); }
+        }
+        
+
+
+        public MainViewModel()
+        {
+            HelloWorld = IsInDesignMode
+                ? "Runs in design mode"
+                : "Runs in runtime mode";
+        }
+    }
+}
